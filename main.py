10b08e18ac317349bb7d71362785d967e8eb8e9a
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2024-04-30 00:14:33 +0300

    ..

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import time
-
 # Start Chrome WebDriver with options
-
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 10)
 
 # Open the URL
 driver.get("https://kefeta-dhis.amref.org/dhis-web-commons/security/login.action")
-time.sleep(5)
 
 # Wait for the username field to be visible
 username = wait.until(EC.visibility_of_element_located((By.ID, "j_username")))
@@ -31,25 +26,31 @@ password.send_keys("P@ssword1")
 signin = wait.until(EC.element_to_be_clickable((By.ID, "submit")))
 signin.click()
 
+# Define a function to handle StaleElementReferenceException and retry the operation
+def retry_click(element):
+    try:
+        element.click()
+    except StaleElementReferenceException:
+        retry_click(element)
 
-menu= driver.find_element(By.XPATH,'//a[@data-test="headerbar-apps-icon"]')
-menu.click()
 
-
-tracker_capture= driver.find_element(By.XPATH,'//a[@href="../api/../dhis-web-tracker-capture/index.action"]')
-tracker_capture.click()
+menu= wait.until(EC.visibility_of_element_located((By.XPATH,'//a[@data-test="headerbar-apps-icon"]')))
+retry_click(menu)
+# Click on the Tracker Capture link
+tracker_capture = wait.until(EC.visibility_of_element_located((By.XPATH,'//a[@href="../api/../dhis-web-tracker-capture/index.action"]')))
+retry_click(tracker_capture)
 
 time.sleep(5)
 toggle= driver.find_element(By.XPATH, '//span[@class="select2-arrow ui-select-toggle"]')
 toggle.click()
 
 time.sleep(2)
-youth_tally=driver.find_element(By.XPATH, '//div[@class="select2-result-label ui-select-choices-row-inner"]')
-youth_tally.click()
+youth_tally= wait.until(EC.visibility_of_element_located((By.XPATH, '//span[@ng-bind-html="program.displayName | highlight: $select.search"]')))
+retry_click(youth_tally)
 
 time.sleep(5)
 
 register= driver.find_element(By.LINK_TEXT,'Register')
 register.click()
+# Close the WebDriver
 
-time.sleep(5)
